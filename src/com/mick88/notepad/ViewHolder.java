7a111416977f3be32811d16d12d1f888f7d75b96
michaldabski <contact@michaldabski.com>
2014-02-01 15:51:19 +0000

    Populate viewholder when instantiating instead of when getting

--- src/com/mick88/notepad/ViewHolder.java
+++ src/com/mick88/notepad/ViewHolder.java
@@ -2,6 +2,7 @@ package com.mick88.notepad;
 
 import android.util.SparseArray;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class ViewHolder
 {
@@ -11,6 +12,18 @@ public class ViewHolder
 	public ViewHolder(View root)
 	{
 		this.root = root;
+		populateMapping(root);
+	}
+	
+	private void populateMapping(View root)
+	{
+		views.put(root.getId(), root);
+		if (root instanceof ViewGroup)
+		{
+			
+			for (int i=0; i < ((ViewGroup) root).getChildCount(); i++)
+				populateMapping(((ViewGroup) root).getChildAt(i));
+		}
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -18,26 +31,12 @@ public class ViewHolder
 	{
 		T view = (T) views.get(id);
 		if (view == null)
-		{
-			views.put(id, (view = (T) root.findViewById(id)));
-			if (view == null)
-				throw new NullPointerException("Cannot find requested view id "+String.valueOf(id));
-		}
+			throw new NullPointerException("Cannot find requested view id "+String.valueOf(id));
 		return view;
 	}
 	
 	public boolean hasView(int id)
 	{
-		if (views.get(id) != null) return true;
-		else
-		{
-			View view = this.root.findViewById(id);
-			if (view == null) return false;
-			else
-			{
-				views.put(id, view);
-				return true;
-			}
-		}
+		return (views.get(id) != null);
 	}
 }
