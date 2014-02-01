5a9dc1373b12b9c4afdb869f39d7a7cd9de238dd
michaldabski <contact@michaldabski.com>
2014-02-01 13:34:32 +0000

    Add ViewHolder

---
+++ src/com/mick88/notepad/ViewHolder.java
@@ -0,0 +1,43 @@
+package com.mick88.notepad;
+
+import android.util.SparseArray;
+import android.view.View;
+
+public class ViewHolder
+{
+	final View root;
+	final SparseArray<View> views = new SparseArray<View>(3);
+	
+	public ViewHolder(View root)
+	{
+		this.root = root;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T extends View> T getViewById(int id)
+	{
+		T view = (T) views.get(id);
+		if (view == null)
+		{
+			views.put(id, (view = (T) root.findViewById(id)));
+			if (view == null)
+				throw new NullPointerException("Cannot find requested view id "+String.valueOf(id));
+		}
+		return view;
+	}
+	
+	public boolean hasView(int id)
+	{
+		if (views.get(id) != null) return true;
+		else
+		{
+			View view = this.root.findViewById(id);
+			if (view == null) return false;
+			else
+			{
+				views.put(id, view);
+				return true;
+			}
+		}
+	}
+}
